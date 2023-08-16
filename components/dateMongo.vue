bc2680c31d7932012abd4d4cae47e2895e5d1949
CLOPEZPRO <khrystyan881@hotmail.com>
2023-08-16 11:29:46 -0500

    add new component to represent Date of mongo in timezone +0

---
+++ components/dateMongo.vue
@@ -0,0 +1,21 @@
+<script setup>
+const props = defineProps({
+  date: {
+    type: String,
+    default: null,
+  },
+})
+const showDate = computed(() => {
+  if (props.date) {
+    const date = new Date(props.date)
+    date.setMinutes(date.getMinutes() + date.getTimezoneOffset())
+    return date.toLocaleDateString('es-ES', { weekday: 'long', year: 'numeric', month: 'short', day: 'numeric' })
+  }
+
+  return ''
+})
+</script>
+
+<template>
+  <span>{{ showDate }}</span>
+</template>
